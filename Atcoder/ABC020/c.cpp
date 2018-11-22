fc6b3d93fd480207f2892c909689d19140e01aa2
栗原佳輝 <yumyum0073@gmail.com>
2018-11-22 14:04:41 +0900

    sync

---
+++ Atcoder/ABC020/c.cpp
@@ -0,0 +1,39 @@
+/**
+ * @author torith
+ * @date 2018-11-21
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+struct edge {
+    int to;
+    int white;
+    int black;
+};
+
+vector<edge> G[100];
+
+int main(){
+
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int H, W, T; cin >> H >> W >> T;
+    int start, goal;
+
+    for (int i = 0; i < H; ++i) {
+        for (int j = 0; j < W; ++j) {
+            char t; cin >> t;
+            if (t == '.')
+        }
+    }
+
+
+
+    return 0;
+}
+
