c90dfbc2ce3ecbc818691ddfded12ff11f22440e
Yoshiki Kurihara <yumyum0073@gmail.com>
2018-11-27 16:36:06 +0900

    sync

--- Atcoder/ABC017/c.cpp
+++ Atcoder/ABC017/c.cpp
@@ -9,10 +9,34 @@ using namespace std;
 
 typedef long long ll;
 
+struct remain{
+    int l;
+    int r;
+    int s;
+};
+
 int main(){
 
     cin.tie(0);
     ios::sync_with_stdio(false);
+    int N, M; cin >> N >> M;
+
+    vector<remain> remains;
+    for (int i = 0; i < N; ++i) {
+        int l, r, s;
+        cin >> l >> r >> s;
+        remain re = {l, r, s};
+        remains.push_back(re);
+    }
+
+    int dp[100000] = {};
+    bool visted[100000] = {};
+
+    // DPの更新
+    for (int i = 1; i < M; ++i) {
+        if()
+    }
+
 
     return 0;
 }
