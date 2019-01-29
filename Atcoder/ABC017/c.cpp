b0a36821e5e165c10d4e812f9eb15e2dedf71458
栗原佳輝 <yumyum0073@gmail.com>
2019-01-29 10:24:17 +0900

    [ABC017-C] AC

--- Atcoder/ABC017/c.cpp
+++ Atcoder/ABC017/c.cpp
@@ -1,6 +1,7 @@
 /**
  * @author torith
  * @date 2018-11-25
+ * 方針: 全体から最も価値のない区間を使わないことにする
  */
 
 #include <bits/stdc++.h>
@@ -9,34 +10,30 @@ using namespace std;
 
 typedef long long ll;
 
-struct remain{
-    int l;
-    int r;
-    int s;
-};
-
 int main(){
 
     cin.tie(0);
     ios::sync_with_stdio(false);
     int N, M; cin >> N >> M;
 
-    vector<remain> remains;
+    vector<int> a(100000);
+    int sum = 0;
     for (int i = 0; i < N; ++i) {
         int l, r, s;
         cin >> l >> r >> s;
-        remain re = {l, r, s};
-        remains.push_back(re);
+        sum += s;
+        a[l-1] += s;
+        a[r] -= s;
     }
 
-    int dp[100000] = {};
-    bool visted[100000] = {};
-
-    // DPの更新
-    for (int i = 1; i < M; ++i) {
-        if()
+    int ims = 0;
+    int min_sum = 1e9;
+    for (int i = 0; i < M; ++i) {
+        ims += a[i];
+        min_sum = min(ims, min_sum);
     }
 
+    cout << sum - min_sum << "\n";
 
     return 0;
 }
